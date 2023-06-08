a3f8adc5eb3b5c21ee883689251244aa3cd76df3
marcialfps <marcialfco@live.com>
2023-06-09 00:05:08 +0100

    Tailwind adoption

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,13 @@ export default function Header() {
   const [isOpened, setOpened] = useState(false);
 
   return (
-    <header>
-      <div className="header-main">
-        <a className="logo-anchor" href="/">
-          <svg viewBox="0 0 203 27" fill="none">
+    <header className="flex flex-col">
+      <div className="flex items-center justify-between py-2 px-3 border-b-2 border-darkGrey">
+        <a
+          className="no-underline text-white h-4 hover:text-yellow focus:text-yellow"
+          href="/"
+        >
+          <svg className="h-4" viewBox="0 0 203 27" fill="none">
             <path
               d="M3 3.08234L4.15126 0.863194L0.5 3.08234H3ZM46.3369 3.08234H48.8369L45.1857 0.863194L46.3369 3.08234ZM24.6685 14.3236L23.5172 16.5428L24.6685 17.1401L25.8197 16.5428L24.6685 14.3236ZM43.8369 3.08234V26.9176H48.8369V3.08234H43.8369ZM5.5 26.9176V3.08234H0.5V26.9176H5.5ZM1.84874 5.30148L23.5172 16.5428L25.8197 12.1045L4.15126 0.863194L1.84874 5.30148ZM25.8197 16.5428L47.4882 5.30148L45.1857 0.863194L23.5172 12.1045L25.8197 16.5428Z"
               fill="currentColor"
@@ -34,7 +37,7 @@ export default function Header() {
         </a>
 
         <button
-          className="menu"
+          className="border-0 rounded p-0 flex justify-center items-center w-8 h-8 bg-darkGrey hover:bg-grey focus:bg-grey"
           type="button"
           onClick={() => {
             console.log("hey");
@@ -42,14 +45,14 @@ export default function Header() {
           }}
         >
           {isOpened ? (
-            <svg viewBox="0 -960 960 960">
+            <svg className="w-6 h-6 text-white" viewBox="0 -960 960 960">
               <path
                 d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"
                 fill="currentColor"
               />
             </svg>
           ) : (
-            <svg viewBox="0 0 54 36" fill="none">
+            <svg className="w-6 h-6 text-white" viewBox="0 0 54 36" fill="none">
               <path
                 d="M0.75 35.5V31.125H53.25V35.5H0.75ZM0.75 20.1875V15.8125H53.25V20.1875H0.75ZM0.75 4.875V0.5H53.25V4.875H0.75Z"
                 fill="currentColor"
@@ -60,15 +63,30 @@ export default function Header() {
       </div>
 
       {isOpened && (
-        <ul className="nav-container">
-          <li>
-            <a href="/about">About</a>
+        <ul className="list-none flex justify-center gap-12 py-1 m-0 bg-darkGrey">
+          <li className="text-serif text-lg">
+            <a
+              className="no-underline text-white hover:text-yellow focus:text-yellow"
+              href="/about"
+            >
+              About
+            </a>
           </li>
-          <li>
-            <a href="/projects">Projects</a>
+          <li className="text-serif text-lg">
+            <a
+              className="no-underline text-white hover:text-yellow focus:text-yellow"
+              href="/projects"
+            >
+              Projects
+            </a>
           </li>
-          <li>
-            <a href="/blog">Blog</a>
+          <li className="text-serif text-lg">
+            <a
+              className="no-underline text-white hover:text-yellow focus:text-yellow"
+              href="/blog"
+            >
+              Blog
+            </a>
           </li>
         </ul>
       )}
